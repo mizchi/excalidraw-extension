bb77b9bd79e571701326c723bfdaf2aa07871717
mizchi <miz404@gmail.com>
2020-03-17 15:51:48 +0900

    Add state loader

--- src/panel.tsx
+++ src/panel.tsx
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom";
 // @ts-ignore
 import { Excalidraw } from "@mizchi/excalidraw";
 
-function App() {
+function App(props: { initialState: any }) {
+  const [state, setState] = useState<any | null>(props.initialState);
+
+  const [age, setAge] = useState<number>(0);
+
   const [size, setSize] = useState<{ width: number; height: number } | null>(
     null
   );
@@ -40,14 +44,56 @@ function App() {
             background: "#333"
           }}
         >
-          --- excalidraw ---
+          <div>
+            <button
+              onClick={() => {
+                chrome.storage.sync.set({ state1: state }, () => {
+                  console.log("saved");
+                });
+              }}
+            >
+              save[1]
+            </button>
+            <button
+              onClick={() => {
+                chrome.storage.sync.get(["state1"], result => {
+                  setState(result.state1);
+                  setAge(n => n + 1);
+                });
+              }}
+            >
+              load[1]
+            </button>
+            |
+            <button
+              onClick={() => {
+                chrome.storage.sync.set({ state2: state }, () => {
+                  console.log("saved");
+                });
+              }}
+            >
+              save[2]
+            </button>
+            <button
+              onClick={() => {
+                chrome.storage.sync.get(["state2"], result => {
+                  setState(result.state2);
+                  setAge(n => n + 1);
+                });
+              }}
+            >
+              load[2]
+            </button>
+          </div>
         </header>
         <div style={{ width: "100%", height: "calc(100% - 32px)" }}>
           <Excalidraw
+            key={age}
+            initialState={state || undefined}
             width={size.width}
             height={size.height}
             onChange={(appState: any) => {
-              console.log(appState);
+              setState(appState);
             }}
           />
         </div>
@@ -56,4 +102,9 @@ function App() {
   );
 }
 
-ReactDOM.render(<App />, document.querySelector("main"));
+chrome.storage.sync.get(["state1"], result => {
+  ReactDOM.render(
+    <App initialState={result.state1} />,
+    document.querySelector("main")
+  );
+});
